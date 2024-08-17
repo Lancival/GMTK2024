aeeec3302ab00f2d1b79f4bcc516d3d3e704a4b0
Richard Cheng <richard.hokan.cheng@gmail.com>
2024-08-17 15:29:06 -0700

    Spawn tooltip on hover

---
+++ Assets/Scripts/UITooltip.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UITooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+  [Tooltip("Time cursor must hover before tooltip will appear")]
+  [Range(0f, 1f)]
+  [SerializeField] private float hoverTime = 0.5f;
+
+  [Tooltip("Tooltip prefab")]
+  [SerializeField] private GameObject tooltipPrefab;
+
+  private bool hovering = false;
+  private float enterTime = 0f;
+  private GameObject tooltip;
+
+  public void OnPointerEnter(PointerEventData eventData) {
+    hovering = true;
+    enterTime = Time.time;
+  }
+
+  public void OnPointerExit(PointerEventData eventData) {
+    hovering = false;
+    Destroy(tooltip);
+    tooltip = null;
+  }
+
+  void Update() {
+    if (hovering && !tooltip && Time.time - enterTime > hoverTime) {
+      tooltip = Instantiate(tooltipPrefab, transform);
+      tooltip.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+      // TODO: Change tooltip text
+    }
+  }
+
+}
