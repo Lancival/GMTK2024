c0ecb44eadf29e14e23cac543acc8a72f4d38170
bryant-ta <33585269+bryant-ta@users.noreply.github.com>
2024-08-18 19:04:35 -0700

    added assetType filter

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -11,23 +12,21 @@ public class Inventory : MonoBehaviour {
     int level = 1; // TODO: pull from GameManager
 
     void Start() {
-        m_Tabs[0].interactable = false;
-
-        UpdateScrollContent(level);
+        PopulateFishes();
     }
 
     public void PopulateFishes() {
         m_Tabs[0].interactable = false;
         m_Tabs[1].interactable = true;
         ClearScrollContentItems();
-        UpdateScrollContent(level);
+        UpdateScrollContent(level, "Fish");
     }
 
     public void PopulateDecos() {
         m_Tabs[0].interactable = true;
         m_Tabs[1].interactable = false;
         ClearScrollContentItems();
-        UpdateScrollContent(level);
+        UpdateScrollContent(level, "Decoration");
     }
 
     private void ClearScrollContentItems() {
@@ -36,8 +35,8 @@ public class Inventory : MonoBehaviour {
         }
     }
 
-    private void UpdateScrollContent(int stage) {
-        List<StatsDatabase.StatItem> contentItems = StatsDatabase.Items.Where(x => x.stage == stage).ToList();
+    private void UpdateScrollContent(int stage, string assetType) {
+        List<StatsDatabase.StatItem> contentItems = StatsDatabase.Items.Where(x => x.stage == stage && x.assetType == assetType).ToList();
 
         for (int i = 0; i < contentItems.Count; i++) {
             var item = Instantiate(m_ItemPrefab, m_ContentContainer, true);
